bde1c6bfad4ac38c1664d30a0995b3ae58628bae
Bilwa Gutthi <bilwa.gutthi@gmail.com>
2020-02-27 14:07:16 +0530

    Init files added

---
+++ Algorithms/lstm.py
@@ -0,0 +1,61 @@
+'''
+
+    Algorithm for lstm
+    ref:https://medium.com/@gautam.karmakar/manhattan-lstm-model-for-text-similarity-2351f80d72f1
+
+'''
+from Sample_Data.data2 import sample_data
+from Sample_Data.test_data import title,abstract
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+from keras.layers import Input, Embedding, LSTM, Merge
+import keras.backend as K
+from keras.optimizers import Adadelta
+from keras.callbacks import ModelCheckpoint
+
+class Siamese_LSTM:
+    def text_to_word_list(text):
+        text = str(text)
+        text = text.lower()
+        # Clean the text
+        text = re.sub(r”[^A-Za-z0–9^,!.\/’+-=]”, “ “, text)
+        text = re.sub(r”what’s”, “what is “, text)
+        text = re.sub(r”\’s”, “ “, text)
+        text = re.sub(r”\’ve”, “ have “, text)
+        text = re.sub(r”can’t”, “cannot “, text)
+        text = re.sub(r”n’t”, “ not “, text)
+        text = re.sub(r”i’m”, “i am “, text)
+        text = re.sub(r”\’re”, “ are “, text)
+        text = re.sub(r”\’d”, “ would “, text)
+        text = re.sub(r”\’ll”, “ will “, text)
+        text = re.sub(r”,”, “ “, text)
+        text = re.sub(r”\.”, “ “, text)
+        text = re.sub(r”!”, “ ! “, text)
+        text = re.sub(r”\/”, “ “, text)
+        text = re.sub(r”\^”, “ ^ “, text)
+        text = re.sub(r”\+”, “ + “, text)
+        text = re.sub(r”\-”, “ — “, text)
+        text = re.sub(r”\=”, “ = “, text)
+        text = re.sub(r”’”, “ “, text)
+        text = re.sub(r”(\d+)(k)”, r”\g<1>000", text)
+        text = re.sub(r”:”, “ : “, text)
+        text = re.sub(r” e g “, “ eg “, text)
+        text = re.sub(r” b g “, “ bg “, text)
+        text = re.sub(r” u s “, “ american “, text)
+        text = re.sub(r”\0s”, “0”, text)
+        text = re.sub(r” 9 11 “, “911”, text)
+        text = re.sub(r”e — mail”, “email”, text)
+        text = re.sub(r”j k”, “jk”, text)
+        text = re.sub(r”\s{2,}”, “ “, text)
+        text = text.split()
+        return text
+    
+    def embedd_data(sample_data,abstract):
+
+
+    def compare(original_abs, new_abs):
+
+
+    
+
+        
